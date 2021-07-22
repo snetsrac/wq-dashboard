d8d97b37410fda9861a019d514fe68566f7cf1b0
snetsrac <dev@chriscarstens.com>
2021-07-22 15:57:48 +0000

    Import NOAA weather stations

---
+++ stations.js
@@ -0,0 +1,321 @@
+const stations = {
+    "metadata": {
+        "resultset": {
+            "offset": 1,
+            "count": 26,
+            "limit": 1000
+        }
+    },
+    "results": [
+        {
+            "elevation": 5.2,
+            "mindate": "2007-10-01",
+            "maxdate": "2021-06-13",
+            "latitude": 26.575478,
+            "name": "ABERDEEN 3.7 WNW, FL US",
+            "datacoverage": 0.8587,
+            "id": "GHCND:US1FLPB0002",
+            "elevationUnit": "METERS",
+            "longitude": -80.202553
+        },
+        {
+            "elevation": 5.8,
+            "mindate": "2007-10-01",
+            "maxdate": "2021-06-13",
+            "latitude": 26.518,
+            "name": "SUN VALLEY 0.8 SW, FL US",
+            "datacoverage": 0.6352,
+            "id": "GHCND:US1FLPB0007",
+            "elevationUnit": "METERS",
+            "longitude": -80.145
+        },
+        {
+            "elevation": 9.1,
+            "mindate": "2007-12-09",
+            "maxdate": "2021-06-05",
+            "latitude": 26.945819,
+            "name": "JUPITER 7.9 W, FL US",
+            "datacoverage": 0.6429,
+            "id": "GHCND:US1FLPB0009",
+            "elevationUnit": "METERS",
+            "longitude": -80.229858
+        },
+        {
+            "elevation": 11.9,
+            "mindate": "2007-10-26",
+            "maxdate": "2021-06-13",
+            "latitude": 26.607032,
+            "name": "ABERDEEN 4.2 NNW, FL US",
+            "datacoverage": 0.9781,
+            "id": "GHCND:US1FLPB0012",
+            "elevationUnit": "METERS",
+            "longitude": -80.172049
+        },
+        {
+            "elevation": 15.8,
+            "mindate": "2007-11-04",
+            "maxdate": "2021-06-13",
+            "latitude": 26.633057,
+            "name": "LAKE WORTH 1.0 NNW, FL US",
+            "datacoverage": 0.9662,
+            "id": "GHCND:US1FLPB0013",
+            "elevationUnit": "METERS",
+            "longitude": -80.066103
+        },
+        {
+            "elevation": 5.2,
+            "mindate": "2011-05-31",
+            "maxdate": "2021-06-13",
+            "latitude": 26.714038,
+            "name": "ROYAL PALM BEACH 5.0 W, FL US",
+            "datacoverage": 0.6073,
+            "id": "GHCND:US1FLPB0031",
+            "elevationUnit": "METERS",
+            "longitude": -80.30703
+        },
+        {
+            "elevation": 4.3,
+            "mindate": "2012-11-13",
+            "maxdate": "2021-06-13",
+            "latitude": 26.8374,
+            "name": "PAHOKEE 1.6 NE, FL US",
+            "datacoverage": 0.8246,
+            "id": "GHCND:US1FLPB0034",
+            "elevationUnit": "METERS",
+            "longitude": -80.6471
+        },
+        {
+            "elevation": 1.5,
+            "mindate": "2012-01-01",
+            "maxdate": "2021-06-04",
+            "latitude": 26.5838024911087,
+            "name": "LANTANA 0.3 E, FL US",
+            "datacoverage": 0.6094,
+            "id": "GHCND:US1FLPB0036",
+            "elevationUnit": "METERS",
+            "longitude": -80.0506314844475
+        },
+        {
+            "elevation": 4.6,
+            "mindate": "2014-03-23",
+            "maxdate": "2021-06-02",
+            "latitude": 26.901957,
+            "name": "JUPITER 2.0 SW, FL US",
+            "datacoverage": 0.6105,
+            "id": "GHCND:US1FLPB0043",
+            "elevationUnit": "METERS",
+            "longitude": -80.124571
+        },
+        {
+            "elevation": 1.5,
+            "mindate": "2014-03-30",
+            "maxdate": "2021-06-13",
+            "latitude": 26.3984977,
+            "name": "BOCA RATON 2.7 NE, FL US",
+            "datacoverage": 0.8112,
+            "id": "GHCND:US1FLPB0044",
+            "elevationUnit": "METERS",
+            "longitude": -80.0735918
+        },
+        {
+            "elevation": 4.9,
+            "mindate": "2014-04-01",
+            "maxdate": "2021-06-13",
+            "latitude": 26.4419841766357,
+            "name": "DELRAY BEACH 1.6 WSW, FL US",
+            "datacoverage": 0.9787,
+            "id": "GHCND:US1FLPB0061",
+            "elevationUnit": "METERS",
+            "longitude": -80.1105346679688
+        },
+        {
+            "elevation": 4.3,
+            "mindate": "2014-04-08",
+            "maxdate": "2021-06-13",
+            "latitude": 26.4277744293213,
+            "name": "DELRAY BEACH 1.9 SSE, FL US",
+            "datacoverage": 0.8982,
+            "id": "GHCND:US1FLPB0063",
+            "elevationUnit": "METERS",
+            "longitude": -80.0808944702148
+        },
+        {
+            "elevation": 4.6,
+            "mindate": "2014-06-28",
+            "maxdate": "2021-06-06",
+            "latitude": 26.535709,
+            "name": "BOYNTON BEACH 1.3 WNW, FL US",
+            "datacoverage": 0.7701,
+            "id": "GHCND:US1FLPB0070",
+            "elevationUnit": "METERS",
+            "longitude": -80.09982
+        },
+        {
+            "elevation": 11,
+            "mindate": "2014-07-30",
+            "maxdate": "2021-06-13",
+            "latitude": 26.55375,
+            "name": "BOYNTON BEACH 6.2 WNW, FL US",
+            "datacoverage": 0.9865,
+            "id": "GHCND:US1FLPB0072",
+            "elevationUnit": "METERS",
+            "longitude": -80.176418
+        },
+        {
+            "elevation": 9.4,
+            "mindate": "2016-04-11",
+            "maxdate": "2021-06-12",
+            "latitude": 26.5613422393799,
+            "name": "ABERDEEN 1.4 WNW, FL US",
+            "datacoverage": 0.937,
+            "id": "GHCND:US1FLPB0077",
+            "elevationUnit": "METERS",
+            "longitude": -80.1686172485352
+        },
+        {
+            "elevation": 2.1,
+            "mindate": "2016-05-24",
+            "maxdate": "2021-06-13",
+            "latitude": 26.38388,
+            "name": "BOCA RATON 2.1 ENE, FL US",
+            "datacoverage": 0.9513,
+            "id": "GHCND:US1FLPB0079",
+            "elevationUnit": "METERS",
+            "longitude": -80.07553
+        },
+        {
+            "elevation": 4.6,
+            "mindate": "2016-08-10",
+            "maxdate": "2021-06-13",
+            "latitude": 26.35442,
+            "name": "BOCA RATON 3.8 WSW, FL US",
+            "datacoverage": 0.8751,
+            "id": "GHCND:US1FLPB0080",
+            "elevationUnit": "METERS",
+            "longitude": -80.164432
+        },
+        {
+            "elevation": 1.5,
+            "mindate": "2016-09-20",
+            "maxdate": "2021-06-13",
+            "latitude": 26.79701,
+            "name": "PALM BEACH SHORES 1.4 N, FL US",
+            "datacoverage": 0.9867,
+            "id": "GHCND:US1FLPB0081",
+            "elevationUnit": "METERS",
+            "longitude": -80.034979
+        },
+        {
+            "elevation": 7.9,
+            "mindate": "2017-05-02",
+            "maxdate": "2021-06-13",
+            "latitude": 26.6674136,
+            "name": "GOLDEN LAKES 2.4 SSW, FL US",
+            "datacoverage": 0.9089,
+            "id": "GHCND:US1FLPB0087",
+            "elevationUnit": "METERS",
+            "longitude": -80.1785257
+        },
+        {
+            "elevation": 4.3,
+            "mindate": "2018-06-01",
+            "maxdate": "2021-06-11",
+            "latitude": 26.723492,
+            "name": "BELLE GLADE 2.6 N, FL US",
+            "datacoverage": 0.9124,
+            "id": "GHCND:US1FLPB0096",
+            "elevationUnit": "METERS",
+            "longitude": -80.666527
+        },
+        {
+            "elevation": 6.1,
+            "mindate": "2018-08-19",
+            "maxdate": "2021-06-13",
+            "latitude": 26.74734,
+            "name": "ROYAL PALM BEACH 6.9 WNW, FL US",
+            "datacoverage": 0.8835,
+            "id": "GHCND:US1FLPB0098",
+            "elevationUnit": "METERS",
+            "longitude": -80.3282
+        },
+        {
+            "elevation": 10.1,
+            "mindate": "2019-10-05",
+            "maxdate": "2021-06-13",
+            "latitude": 26.346721,
+            "name": "SANDALFOOT COVE 0.8 NW, FL US",
+            "datacoverage": 0.945,
+            "id": "GHCND:US1FLPB0106",
+            "elevationUnit": "METERS",
+            "longitude": -80.197888
+        },
+        {
+            "elevation": 6.4,
+            "mindate": "1990-10-01",
+            "maxdate": "2021-06-04",
+            "latitude": 26.4985,
+            "name": "LOXAHATCHEE NWR, FL US",
+            "datacoverage": 0.9355,
+            "id": "GHCND:USC00085184",
+            "elevationUnit": "METERS",
+            "longitude": -80.216
+        },
+        {
+            "elevation": 6.1,
+            "mindate": "2002-11-01",
+            "maxdate": "2021-06-11",
+            "latitude": 26.8261,
+            "name": "PALM BEACH GARDENS, FL US",
+            "datacoverage": 0.9831,
+            "id": "GHCND:USC00086764",
+            "elevationUnit": "METERS",
+            "longitude": -80.1488
+        },
+        {
+            "elevation": 6.1,
+            "mindate": "2002-11-01",
+            "maxdate": "2021-06-11",
+            "latitude": 26.47,
+            "name": "SOUTH BAY 15 S, FL US",
+            "datacoverage": 0.9716,
+            "id": "GHCND:USC00088368",
+            "elevationUnit": "METERS",
+            "longitude": -80.6402
+        },
+        {
+            "elevation": 5.8,
+            "mindate": "1938-07-01",
+            "maxdate": "2021-06-13",
+            "latitude": 26.6847,
+            "name": "WEST PALM BEACH INTERNATIONAL AIRPORT, FL US",
+            "datacoverage": 0.9818,
+            "id": "GHCND:USW00012844",
+            "elevationUnit": "METERS",
+            "longitude": -80.0994
+        }
+    ]
+}
+
+const fs = require('fs');
+fs.writeFileSync('./stations.json', 
+  JSON.stringify(
+    stations.results.map((station) => {
+      return {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [station.longitude, station.latitude]
+        },
+        properties: {
+          id: station.id,
+          name: station.name,
+          datacoverage: station.datacoverage,
+          mindate: station.mindate,
+          maxdate: station.maxdate,
+          elevation: station.elevation,
+          elevationUnit: station.elevationUnit
+        }
+      };
+    })
+  )
+);
