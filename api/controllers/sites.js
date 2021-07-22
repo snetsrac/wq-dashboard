04342941bf8e773ab80147e16aea95ef2cb25f8a
snetsrac <dev@chriscarstens.com>
2021-07-22 16:15:38 +0000

    Remove null dbkey from response

--- api/controllers/sites.js
+++ api/controllers/sites.js
@@ -2,7 +2,12 @@ const { site } = require('../sequelize').models;
 
 module.exports.getSites = async (req, res, next) => {
   const sites = await site.findAll({
-    attributes: ['name', 'dbkey', 'type', 'longitude', 'latitude']
+    attributes: ['name', 'dbkey', 'type', 'longitude', 'latitude'],
+    raw: true
+  });
+
+  sites.forEach((site) => {
+    if (site.dbkey == null) delete site.dbkey;
   });
 
   res.json(sites);
